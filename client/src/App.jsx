98d8fda1e96889a8ce7cb170ecc62717a88b594b
Jason Chang <jasonchangji@hotmail.com>
2024-05-11 13:22:48 +0800

    add redux persist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/Header'
 
+
 export default function App() {
   return <BrowserRouter>
   <Header/>
